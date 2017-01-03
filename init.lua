ff6a8311faa0b6432d8d0ec00d31a36b9ae73fdd
rubenwardy <rubenwardy@gmail.com>
2017-01-03 18:44:13 +0000

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,16 @@
+function default.can_grow(pos)
+	local node_under = minetest.get_node_or_nil({x = pos.x, y = pos.y - 1, z = pos.z})
+	if not node_under then
+		return false
+	end
+	local name_under = node_under.name
+	local is_soil = minetest.get_item_group(name_under, "soil")
+	if is_soil == 0 then
+		return false
+	end
+	local light_level = minetest.get_node_light(pos)
+	if not light_level or light_level < 12 then
+		return false
+	end
+	return true
+end
